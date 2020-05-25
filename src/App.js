8c4be589d8ad44323cac8e89f98d814307fba1c1
Ajnas aju <ajnasaju3@gmail.com>
2020-05-25 21:30:53 +0530

    basic components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.module.css';
 import {Cards,Chart,CountryPicker} from './Components/index'
 class App extends Component{
   render(){
     return(
-      <div>
-        <h1>App</h1>
+      <div className={classes.container}>
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
       </div>
     )
   }
