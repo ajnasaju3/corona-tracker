7b1d117ae92846315cd39dfaa43192381bd94666
Ajnas aju <ajnasaju3@gmail.com>
2020-05-25 21:21:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Cards,Chart,CountryPicker} from './Components/index'
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>App</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
