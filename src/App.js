fffeaaab50e03c1500ed34e52b9c11fc5484d644
Ajnas aju <ajnasaju3@gmail.com>
2020-05-26 16:25:19 +0530

    finished components

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import {Cards,Chart,CountryPicker} from './Components/index'
 import {fetchData} from './api/index'
 class App extends Component{
   state={
-    data:{}
+    data:{},
+    country:'',
   }
   async componentDidMount(){
     const fetchedData=await fetchData()
     this.setState({data:fetchedData})
+  }
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
   }
   render(){
     const data=this.state.data
     return(
       <div className={classes.container}>
         <Cards data={data}/>
-        <CountryPicker/>
-        <Chart/>
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={this.state.country}/>
       </div>
     )
   }
